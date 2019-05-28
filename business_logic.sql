b24363bdf44d4d05bb2240c7512683b06bb6b158
reacheight <reacheight8@gmail.com>
2019-05-28 18:46:58 +0300

    added some busines logic examples

---
+++ business_logic.sql
@@ -0,0 +1,28 @@
+USE coffee_company;
+GO
+
+CREATE PROCEDURE UpdateOrDeleteOrder
+@var_upd_del AS CHAR(3),
+@var_order_id AS INTEGER
+AS
+BEGIN
+    IF @var_upd_del = 'UPD'
+        UPDATE Company_Order SET Is_Completed = 1 WHERE Company_Order_ID = @var_order_id;
+    IF @var_upd_del = 'DEL'
+        DELETE FROM Company_Order WHERE Company_Order_ID = @var_order_id;
+END;
+
+GO
+
+CREATE FUNCTION GetPlantationBatchesCount (@plantation_id INTEGER)
+RETURNS INTEGER
+AS
+BEGIN
+    DECLARE @Result INTEGER;
+    SELECT @Result = COUNT(*) FROM Order_Batch JOIN Plantation_Product
+        ON Order_Batch.Product_ID = Plantation_Product.Plantation_Product_ID
+        WHERE Order_Batch.Is_Arrived = 0;
+    RETURN @Result;
+END;
+
+GO
